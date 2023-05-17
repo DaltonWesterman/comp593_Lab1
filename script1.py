6da4519c7d38030a777c5d2e33ad286d6d9d2f5e
Dalton Westerman <133789471+DaltonWesterman@users.noreply.github.com>
2023-05-17 13:21:07 -0700

    created script files
    added code about this semester and the improved changes to the scripting class.

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("This class is way better now that we have it in person")
+print("I'm very excited to start learning and diving deeper into code")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("This semester is going to be a lot better than last")
+print("It's going to be sad when you leave half way through and another prof takes over :(")
