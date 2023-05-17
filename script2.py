1ff50ffcccd23c0bb50d20fb43108e373fb1bb9b
Dalton Westerman <133789471+DaltonWesterman@users.noreply.github.com>
2023-05-17 14:04:32 -0700

    Modified both script.py files in the repo clone
    -Deleted a print statement from script1.py
    -Added a print statement to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # print lines on the good news and bad news on this new scripting semester
 print("This semester is going to be a lot better than last")
 print("It's going to be sad when you leave half way through and another prof takes over :(")
+print("Scripting is the most important class in our entire CSI program")
