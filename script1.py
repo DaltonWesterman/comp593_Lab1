b911c461044ff65a68be9866952e1fde53747c49
Dalton Westerman <133789471+DaltonWesterman@users.noreply.github.com>
2023-05-17 13:29:18 -0700

    added comment to script1.py
    Added a comment to the top of script1.py to allow user to tell what script is about.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
+#print lines on the improved class structure
 print("This class is way better now that we have it in person")
 print("I'm very excited to start learning and diving deeper into code")
