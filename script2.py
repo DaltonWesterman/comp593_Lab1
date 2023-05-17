a8b7b36868b564c0199d2977e7fb3b99864800bb
Dalton Westerman <133789471+DaltonWesterman@users.noreply.github.com>
2023-05-17 13:43:57 -0700

    put removed comment back into script2.py
    comment was unintentionally removed by revert

--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# print lines on the good news and bad news on this new scripting semester
 print("This semester is going to be a lot better than last")
 print("It's going to be sad when you leave half way through and another prof takes over :(")
