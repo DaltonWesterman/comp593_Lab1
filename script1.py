b239c10ba4ed9a98d53a33151874f785012a370b
Dalton Westerman <133789471+DaltonWesterman@users.noreply.github.com>
2023-05-17 13:36:12 -0700

    modified both script files
    -Added comment to script2.py
    -Deleted print from script1.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #print lines on the improved class structure
 print("This class is way better now that we have it in person")
-print("I'm very excited to start learning and diving deeper into code")
